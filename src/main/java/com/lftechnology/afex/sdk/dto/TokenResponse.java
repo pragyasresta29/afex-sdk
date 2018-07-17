e26a4ecd78750b082a72e04349e8be16671dd13b
Pragya Shrestha <pragyashrestha@lftechnology.com>
2018-07-17 12:20:17 +0545

    Add dto for token api response

---
+++ src/main/java/com/lftechnology/afex/sdk/dto/TokenResponse.java
@@ -0,0 +1,74 @@
+package com.lftechnology.afex.sdk.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class TokenResponse {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("expires_in")
+    private String expiresIn;
+
+    @JsonProperty(".issued")
+    private String issued;
+
+    @JsonProperty(".expires")
+    private String expires;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public String getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(String expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+
+    public String getIssued() {
+        return issued;
+    }
+
+    public void setIssued(String issued) {
+        this.issued = issued;
+    }
+
+    public String getExpires() {
+        return expires;
+    }
+
+    public void setExpires(String expires) {
+        this.expires = expires;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("TokenResponse{");
+        sb.append("accessToken='").append(accessToken).append('\'');
+        sb.append(", tokenType='").append(tokenType).append('\'');
+        sb.append(", expiresIn='").append(expiresIn).append('\'');
+        sb.append(", issued='").append(issued).append('\'');
+        sb.append(", expires='").append(expires).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
